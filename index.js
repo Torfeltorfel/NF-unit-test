c117583a4f99eb56a4006fcc0311aa529d12e4f2
Torben Feldmann <torben.feldmann1@gmail.com>
2021-10-18 11:58:26 +0200

    initial

---
+++ index.js
@@ -0,0 +1,5 @@
+import { getGreeting } from './getGreetings.js'
+import { replaceName } from './replaceName.js'
+
+console.log(replaceName('Hello Jane', 'Jane', 'John'))
+console.log(replaceName('Jane', 'Jane', 'John'))
