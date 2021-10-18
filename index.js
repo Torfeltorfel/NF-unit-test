a26318933b4ab5b7e6ad3286a67aaa201e407653
Torben Feldmann <torben.feldmann1@gmail.com>
2021-10-18 12:03:44 +0200

    add nodemon & script in package.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { getGreeting } from './getGreetings.js'
 import { replaceName } from './replaceName.js'
 
+console.log('hey')
 console.log(replaceName('Hello Jane', 'Jane', 'John'))
 console.log(replaceName('Jane', 'Jane', 'John'))
